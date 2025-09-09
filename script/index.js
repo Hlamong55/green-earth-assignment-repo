5bb5c021aa6f33c9b6e58942298b38a6f97cbe24
Hlamong55 <bijoymarma55@gmail.com>
2025-09-09 22:23:21 +0600

    plants by category completed

--- script/index.js
+++ script/index.js
@@ -1,13 +1,51 @@
-
+// get categories
 const loadCategories = () => {
     fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
     .then((acData) => displayCategories(acData.categories));
 };
 
+
+
+
+// get plant by category
+const loadCtgPlant = (id) => {
+    const url = `https://openapi.programming-hero.com/api/category/${id}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCtgPlant(data.plants));
+};
+
+const displayCtgPlant = (ctgPlants) => {
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+
+    for(let ctgPlant of ctgPlants){
+
+         const cateDiv = document.createElement("div");
+         cateDiv.innerHTML = `
+            <div class="bg-white space-y-3 p-4">
+                    <img class="w-full h-60 object-cover rounded-lg" src="${ctgPlant.image}" alt="">
+                    <h2 class="text-lg font-semibold">${ctgPlant.name}</h2>
+                    <p class="text-gray-700 line-clamp-3 text-sm">${ctgPlant.description}</p>
+                    <div class="flex justify-between text-base font-medium">
+                        <h2 class="bg-[#DCFCE7] px-3 py-1 rounded-2xl text-green-800">${ctgPlant.category}</h2>
+                        <h2>৳${ctgPlant.price}</h2>
+                    </div>
+                    <button class="bg-green-700 text-white w-full  py-2 rounded-full text-lg font-medium  hover:bg-slate-200 hover:text-green-700 border border-green-700 transition duration-30">Add to Cart</button>
+            </div>
+        `;
+        cardContainer.append(cateDiv);
+    }
+};
+
+
+
+
+// display get categories
 const displayCategories = (allCategories) => {
     const ctgContainer = document.getElementById("ctg-container");
-    ctgContainer.innerHTML = `<h2 class="font-semibold text-xl ml-2 mb-2">Categories</h2>`;
+    ctgContainer.innerHTML = `<h2 class="font-semibold text-xl ml-3 mb-2">Categories</h2>`;
 
 
     for(let allCategory of allCategories) {
@@ -15,7 +53,7 @@ const displayCategories = (allCategories) => {
         const ctgDiv = document.createElement("div");
         ctgDiv.innerHTML = `
             
-            <button class="text-lg hover:bg-green-700 hover:text-white transition rounded w-2/3 md:w-full text-left md:pl-2 py-1 ">${allCategory.category_name}</button>
+            <button onclick="loadCtgPlant(${allCategory.id})" class="block w-full text-lg hover:bg-green-700 hover:text-white transition  rounded  py-1 md:px-5">${allCategory.category_name}</button>
         `;
 
         ctgContainer.append(ctgDiv);
@@ -28,7 +66,7 @@ loadCategories();
 
 
 
-
+// get all plants
 const loadAllPlants = () => {
     fetch("https://openapi.programming-hero.com/api/plants")
     .then((res) => res.json())
@@ -43,8 +81,8 @@ const displayAllPlants = (plants) => {
 
         const cardDiv = document.createElement("div");
         cardDiv.innerHTML = `
-            <div class="bg-white space-y-3 p-3">
-                    <img class="w-full h-50 object-cover rounded-lg" src="${plant.image}" alt="">
+            <div class="bg-white space-y-3 p-4">
+                    <img class="w-full h-60 object-cover rounded-lg" src="${plant.image}" alt="">
                     <h2 class="text-lg font-semibold">${plant.name}</h2>
                     <p class="text-gray-700 line-clamp-3 text-sm">${plant.description}</p>
                     <div class="flex justify-between text-base font-medium">
